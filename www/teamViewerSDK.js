a6ed9d5c2cc27c91ae901ee74335ba5c7b9f44dd
web-slinger <matw2420@outlook.com>
2017-09-14 15:27:23 +0100

    args length error w/cordova

--- www/teamViewerSDK.js
+++ www/teamViewerSDK.js
@@ -1,27 +1,24 @@
 module.exports =
 {
-    openSessionWithConfigurationId: function(token, configurationId, name, description, successCallback, failureCallback)
-    {
+    openSessionWithConfigurationId: function(token, configurationId, name, description, successCallback, failureCallback){
       cordova.exec(successCallback,
                    failureCallback,
                    "TeamViewerSDK",
                    "openSessionWithConfigurationId",
                    [token, configurationId, name, description]);
     },
-    openSessionWithSessionCode: function(token, sessionCode, successCallback, failureCallback)
-    {
+    openSessionWithSessionCode: function(token, sessionCode, successCallback, failureCallback){
       cordova.exec(successCallback,
                    failureCallback,
                    "TeamViewerSDK",
                    "openSessionWithSessionCode",
                    [token, sessionCode]);
     },
-    closeCurrentSession: function(successcallback, failureCallback)
-    {
+    closeCurrentSession: function(successcallback, failureCallback){
       cordova.exec(successcallback,
                    failureCallback,
                    "TeamViewerSDK",
                    "closeCurrentSession",
-                   null);
+                   []);
     }
 };
